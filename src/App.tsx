d99dc5d62c3482366c33e23b3e01b0b1ef35e037
Remi van der Laan <R.M.vanderlaan@student.tudelft.nl>
2020-11-28 11:24:45 +0100

    Setting up rebass...

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ThemeProvider } from "emotion-theming";
+import theme from "@rebass/preset";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { Box, Card, Image, Heading, Text } from "rebass";
+
+const DemoComponent = ({ image, title, description }: any) => (
+  <Box width={256}>
+    <Card
+      sx={{
+        p: 1,
+        borderRadius: 2,
+        boxShadow: "0 0 16px rgba(0, 0, 0, .25)",
+      }}
+    >
+      <Image src={image} />
+      <Box px={2}>
+        <Heading as="h3">{title}</Heading>
+        <Text fontSize={0}>{description}</Text>
+      </Box>
+    </Card>
+  </Box>
+);
+
+const App = (props: any) => (
+  <ThemeProvider theme={theme}>
+    {/* {props.children} */}
+    <DemoComponent title="Hello world" description=":D" />
+  </ThemeProvider>
+);
 
 export default App;
