519a8862122223af9426762e3dc0a75fd0b5ac38
Remi van der Laan <R.M.vanderlaan@student.tudelft.nl>
2020-11-30 23:11:18 +0100

    Fall back to JSON file if ravi is not hosting the server on his laptop bootleg-style

--- src/discussion/api-adapter.ts
+++ src/discussion/api-adapter.ts
@@ -1,9 +1,19 @@
 import { API_URL } from "../config";
 import { IUser, IPost, PostData, unifyComment, findCreator, IComment } from "./mock-data";
 
+import FallbackPosts from '../data-fallback/dummy_data1.json';
+import FallbackUsers from '../data-fallback/users.json';
+
 export async function fetchDiscussionData(id: string): Promise<PostData> {
-  const userData: { data: IUser[] } = await (await fetch(`${API_URL}/users`)).json();
-  const postData: { data: IPost[] } = await (await fetch(`${API_URL}/posts`)).json();
+  let userData: { data: IUser[] };
+  let postData: { data: IPost[] };
+  try {
+    userData = await (await fetch(`${API_URL}/users`)).json();
+    postData = await (await fetch(`${API_URL}/posts`)).json();
+  } catch (e) {
+    postData = FallbackPosts as any;
+    userData = FallbackUsers as any;
+  }
   const post = postData.data.find(p => p.id === id);
   if (!post) throw new Error('Post not found!');
 
@@ -16,9 +26,15 @@ export async function fetchDiscussionData(id: string): Promise<PostData> {
 
 
 export async function fetchPostOverview(): Promise<PostData[]> {
-  const userData: { data: IUser[] } = await (await fetch(`${API_URL}/users`)).json();
-  const postData: { data: IPost[] } = await (await fetch(`${API_URL}/posts`)).json();
-
+  let userData: { data: IUser[] };
+  let postData: { data: IPost[] };
+  try {
+    userData = await (await fetch(`${API_URL}/users`)).json();
+    postData = await (await fetch(`${API_URL}/posts`)).json();
+  } catch (e) {
+    postData = FallbackPosts as any;
+    userData = FallbackUsers as any;
+  }
   return postData.data.map(post => ({
     ...post,
     creator: findCreator(post.creatorId, userData.data),
